1db68e23d05f247d58d3b46d0a741d1b49011bf3
Punmanat <60070053@kmitl.ac.th>
2019-11-05 16:37:48 +0700

    Eureka client

--- product-service/server/server.js
+++ product-service/server/server.js
@@ -4,6 +4,33 @@ const port = process.env.PORT | 8080
 require('./database/db')
 const productRouter = require('./router/product')
 
+// ------------------ Eureka Config --------------------------------------------
+
+const Eureka = require('eureka-js-client').Eureka;
+
+const eureka = new Eureka({
+  instance: {
+    app: 'product-service',
+    hostName: 'localhost',
+    ipAddr: '127.0.0.1',
+    statusPageUrl: 'http://localhost:'+PORT,
+    port: {
+      '$': 8080,
+      '@enabled': 'true',
+    },
+    vipAddress: 'localhost',
+    dataCenterInfo: {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      name: 'MyOwn',
+    }
+  },
+  eureka: {
+    host: 'https://eureka02.appspot.com',
+    port: 80,
+    servicePath: '/eureka/apps/'
+  }
+});
+
 // Router
 app.use(express.json());
 app.use(productRouter)
