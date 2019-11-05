e85f6a8ae36fcac43f01569dc625db98f0c1a1d4
Punmanat <60070053@kmitl.ac.th>
2019-11-05 17:14:25 +0700

    Add server.js

--- product-service/server/server.js
+++ product-service/server/server.js
@@ -1,41 +1,71 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT | 8080
-require('./database/db')
-const productRouter = require('./router/product')
+const express = require("express");
+const app = express();
+const port = process.env.PORT | 8080;
+require("./database/db");
+const productRouter = require("./router/product");
 
 // ------------------ Eureka Config --------------------------------------------
 
-const Eureka = require('eureka-js-client').Eureka;
+// const Eureka = require('eureka-js-client').Eureka;
+
+// const eureka = new Eureka({
+//   instance: {
+//     app: 'product-service',
+//     hostName: 'localhost',
+//     ipAddr: '127.0.0.1',
+//     statusPageUrl: 'http://localhost:'+port,
+//     port: {
+//       '$': 8080,
+//       '@enabled': 'true',
+//     },
+//     vipAddress: 'localhost',
+//     dataCenterInfo: {
+//       '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+//       name: 'MyOwn',
+//     }
+//   },
+//   eureka: {
+//     host: 'https://eureka02.appspot.com',
+//     port: 80,
+//     servicePath: '/eureka/apps/'
+//   }
+// });
+const Eureka = require("eureka-js-client").Eureka;
 
 const eureka = new Eureka({
   instance: {
-    app: 'product-service',
-    hostName: 'localhost',
-    ipAddr: '127.0.0.1',
-    statusPageUrl: 'http://localhost:'+PORT,
+    app: "product-service",
+    hostName: "localhost",
+    ipAddr: "127.0.0.1",
     port: {
-      '$': 8080,
-      '@enabled': 'true',
+      $: 8080,
+      "@enabled": "true"
     },
-    vipAddress: 'localhost',
+    vipAddress: "authservice",
+    statusPageUrl: "https://vocal-pathway-257309.appspot.com",
     dataCenterInfo: {
-      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-      name: 'MyOwn',
-    }
+      "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
+      name: "MyOwn"
+    },
+    registerWithEureka: true,
+    fetchRegistry: true
   },
   eureka: {
-    host: 'https://eureka02.appspot.com',
-    port: 80,
-    servicePath: '/eureka/apps/'
+    host: "eureka02.appspot.com",
+    port:80,
+    servicePath: "/eureka/app"
   }
 });
+// eureka.logger.level("debug");
+// eureka.start(function(error) {
+//   console.log(error || "complete");
+// });
 
 // Router
 app.use(express.json());
-app.use(productRouter)
+app.use(productRouter);
 
 // Create server
-app.listen(port, ()=>{
-    console.log("Start on port " + port)
-})
+app.listen(port, () => {
+  console.log("Start on port " + port);
+});
