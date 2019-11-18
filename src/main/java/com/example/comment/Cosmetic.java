28a94e6fd43a96983f10197c968b0e369736b389
Narongded <Narongded2554@gmail.com>
2019-11-18 16:51:41 +0700

    Narongded
    Comment-service

---
+++ src/main/java/com/example/comment/Cosmetic.java
@@ -0,0 +1,46 @@
+package com.example.comment;
+
+
+@Entity
+public class Cosmetic {
+
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private String id;
+    private String name;
+    private int    amount;
+
+    public Cosmetic(){
+
+    }
+    
+    public Cosmetic(String id, String name, int amount){
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+}
