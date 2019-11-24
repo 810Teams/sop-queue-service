41e7ba610774d1f9385c6d86fd2e6f7d10e3a541
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2019-11-24 10:48:00 +0700

    :recycle: Refactors

--- reservation-service/src/main/java/com/teams810/reservation/api/controllers/ReservationController.java
+++ reservation-service/src/main/java/com/teams810/reservation/api/controllers/ReservationController.java
@@ -2,9 +2,7 @@ package com.teams810.reservation.api.controllers;
 
 import com.teams810.reservation.api.discovery.ServiceDiscoveryClient;
 import com.teams810.reservation.api.entities.Reservation;
-import com.teams810.reservation.api.entities.TimePeriod;
 import com.teams810.reservation.api.entities.service.Item;
-import com.teams810.reservation.api.entities.service.Shop;
 import com.teams810.reservation.api.entities.service.User;
 import com.teams810.reservation.api.exceptions.InvalidStatusFlowException;
 import com.teams810.reservation.api.repositories.ReservationRepository;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -44,8 +41,7 @@ public class ReservationController {
 
             return new ResponseEntity<Reservation>(reservation, HttpStatus.OK);
         } catch (HttpClientErrorException e) {
-            // UNAUTHORIZED
-            return null;
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
 
@@ -80,67 +76,41 @@ public class ReservationController {
             } else if (role.equals("shop") && checkIfIsReservationOfShop(reservation, token)) {
                 return new ResponseEntity<Reservation>(reservation, HttpStatus.OK);
             } else {
-                // UNAUTHORIZED
-                return null;
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
             }
         } catch (NullPointerException | HttpClientErrorException e) {
-            // NOT_FOUND
-            return null;
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
-    @RequestMapping(value = "/reservation/user/{userId}", method = RequestMethod.POST)
+    @RequestMapping(value = "/reservation/me", method = RequestMethod.POST)
     public ResponseEntity<List<Reservation>> userReservation(
-            @PathVariable("userId") String userId,
             @RequestHeader("Authorization") String token
     ) {
         // Operation: Get all reservations of a certain user.
-        // Returns: Reservation list of a cart
-        List<Reservation> reservationList = new ArrayList<Reservation>();
-
-        try {
-            if (userId.equals(discovery.verifyUser(token).getUsername())) {
-                for (Reservation r : repository.findAll()) {
-                    if (r.getUserId().equals(userId)) {
-                        reservationList.add(r);
-                    }
-                }
-                return new ResponseEntity<List<Reservation>>(reservationList, HttpStatus.OK);
-            } else {
-                // UNAUTHORIZED
-                return null;
-            }
-        } catch (HttpClientErrorException e) {
-            // NOT_FOUND
-            return null;
-        }
-    }
-
-    @RequestMapping(value = "/reservation/shop/{userId}", method = RequestMethod.POST)
-    public ResponseEntity<List<Reservation>> shopReservation(
-            @PathVariable("userId") String userId,
-            @RequestHeader("Authorization") String token
-    ) {
-        // Operation: Get all reservations of a certain user.
-        // Returns: Reservation list of a cart
+        // Returns: Reservation list
         List<Reservation> reservationList = new ArrayList<Reservation>();
         User user = discovery.verifyUser(token);
 
         try {
-            if (user.getRole().equals("shop") && userId.equals(user.getUsername())) {
+            if (user.getRole().equals("customer")) {
+                for (Reservation r : repository.findAll()) {
+                    if (r.getUserId().equals(user.getUsername())) {
+                        reservationList.add(r);
+                    }
+                }
+            } else if (user.getRole().equals("shop")) {
                 for (Reservation r : repository.findAll()) {
                     if (checkIfIsReservationOfShop(r, token)) {
                         reservationList.add(r);
                     }
                 }
-                return new ResponseEntity<List<Reservation>>(reservationList, HttpStatus.OK);
             } else {
-                // UNAUTHORIZED
-                return null;
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
+            return new ResponseEntity<List<Reservation>>(reservationList, HttpStatus.OK);
         } catch (HttpClientErrorException e) {
-            // NOT_FOUND
-            return null;
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -161,52 +131,48 @@ public class ReservationController {
             if (role.equals("customer") && reservation.getUserId().equals(userId)) {
                 reservation.cancelByUser();
             } else if (role.equals("shop") && checkIfIsReservationOfShop(reservation, token)) {
-                reservation.cancelByShop();
+                try {
+                    reservation.checkMissed();
+                } catch (InvalidStatusFlowException e) {
+                    reservation.cancelByShop();
+                }
             } else {
-                // UNAUTHORIZED
-                return null;
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
             }
             repository.save(reservation);
 
             return new ResponseEntity<Reservation>(repository.findById(id), HttpStatus.OK);
-        } catch (NullPointerException | HttpClientErrorException | InvalidStatusFlowException e) {
-            // NOT_FOUND | NOT_FOUND | INTERNAL_SERVER_ERROR
-            return null;
+        } catch (NullPointerException | HttpClientErrorException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (InvalidStatusFlowException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @RequestMapping(value = "/reservation/id/{id}/{check}", method = RequestMethod.POST)
+    @RequestMapping(value = "/reservation/id/{id}/check", method = RequestMethod.POST)
     public ResponseEntity<Reservation> checkReservation(
             @PathVariable("id") Long id,
-            @PathVariable("check") String check, // success | missed
             @RequestHeader("Authorization") String token
     ) {
-        // Operation: Cancel a reservation, changes reservation status.
-        // Returns: Cancelled reservation
+        // Operation: Checks a reservation, changes reservation status.
+        // Returns: Checked Reservation
         Reservation reservation = repository.findById(id);
         String role = discovery.verifyUser(token).getRole();
 
         try {
             if (role.equals("shop") && checkIfIsReservationOfShop(reservation, token)) {
-                if (check.equals("success")) {
-                    reservation.checkSuccess();
-                } else if (check.equals("missed")) {
-                    reservation.checkMissed();
-                } else {
-                    // BAD_REQUEST
-                    return null;
-                }
+                reservation.checkSuccess();
             } else {
-                // UNAUTHORIZED
                 return null;
             }
 
             repository.save(reservation);
 
             return new ResponseEntity<Reservation>(repository.findById(id), HttpStatus.OK);
-        } catch (NullPointerException | HttpClientErrorException | InvalidStatusFlowException e) {
-            // NOT_FOUND | NOT_FOUND | INTERNAL_SERVER_ERROR
-            return null;
+        } catch (NullPointerException | HttpClientErrorException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (InvalidStatusFlowException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -214,7 +180,7 @@ public class ReservationController {
         // Operation: Check if a reservation is a reservation of a certain shop by shop token
         // Returns: boolean
         for (Object i : discovery.getShopItems(token).getItems()) {
-            if (reservation.getItemData().get(0).getItemId().equals(((Item) i).get_id())) {
+            if (reservation.getItems().get(0).getItemId().equals(((Item) i).get_id())) {
                 return true;
             }
         }
--- reservation-service/src/main/java/com/teams810/reservation/api/entities/Reservation.java
+++ reservation-service/src/main/java/com/teams810/reservation/api/entities/Reservation.java
@@ -14,7 +14,7 @@ public class Reservation {
     private String userId;
     @ElementCollection
     @CollectionTable(name="listOfItemData")
-    private List<ItemData> itemData;
+    private List<ItemData> items;
     private TimePeriod timePeriod;
     private ReservationStatus status;
     private String message;
@@ -25,13 +25,13 @@ public class Reservation {
         this.message = "";
     }
 
-    public Reservation(String userId, List<ItemData> itemData, TimePeriod timePeriod) {
-        this(userId, itemData, timePeriod, "");
+    public Reservation(String userId, List<ItemData> items, TimePeriod timePeriod) {
+        this(userId, items, timePeriod, "");
     }
 
-    public Reservation(String userId, List<ItemData> itemData, TimePeriod timePeriod, String message) {
+    public Reservation(String userId, List<ItemData> items, TimePeriod timePeriod, String message) {
         this.userId = userId;
-        this.itemData = itemData;
+        this.items = items;
         this.timePeriod = timePeriod;
         this.message = message;
         this.status = ReservationStatus.WAITING;
@@ -39,10 +39,6 @@ public class Reservation {
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getUserId() {
@@ -53,36 +49,20 @@ public class Reservation {
         this.userId = userId;
     }
 
-    public List<ItemData> getItemData() {
-        return itemData;
-    }
-
-    public void setItemData(List<ItemData> itemData) {
-        this.itemData = itemData;
+    public List<ItemData> getItems() {
+        return items;
     }
 
     public TimePeriod getTimePeriod() {
         return timePeriod;
     }
 
-    public void setTimePeriod(TimePeriod timePeriod) {
-        this.timePeriod = timePeriod;
-    }
-
     public ReservationStatus getStatus() {
         return status;
     }
 
-    public void setStatus(ReservationStatus status) {
-        this.status = status;
-    }
-
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
     public void checkSuccess() throws InvalidStatusFlowException {
@@ -97,7 +77,7 @@ public class Reservation {
         // Instance Method: Shop checks reservation as missed
         if (this.status != ReservationStatus.WAITING) {
             throw new InvalidStatusFlowException("Invalid status flow.");
-        } else if (LocalDateTime.now().compareTo(this.timePeriod.getEndDateTime()) < 0) {
+        } else if (LocalDateTime.now().compareTo(this.timePeriod.getEndDateTime()) <= 0) {
             throw new InvalidStatusFlowException("Unable to check as missed before the reserved time.");
         }
         this.status = ReservationStatus.MISSED;
